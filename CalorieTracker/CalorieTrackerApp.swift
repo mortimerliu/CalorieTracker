1c93b428ac40c14189353ec4309d3fe011b5fb02
Hongru Liu <hl3148@columbia.edu>
2023-07-07 23:04:36 -0400

    Initial Commit

---
+++ CalorieTracker/CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by Hongru on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
